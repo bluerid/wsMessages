0f2ccac8f88211634edfaca8612841193cc77a86
Keymaker <palash@itobuz.com>
2018-01-19 16:21:03 +0530

    WS Server implementation

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var url = require('url'),
+    wsServer = require('./wsService')({port: 1337})
+
+process.title = 'ws.server';
+
+
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log(message);
+        }
+    });
+
+    connection.on('close', function(connection) {
+        // close user connection
+    });
+});
